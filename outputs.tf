32fba12ce7698ff5818f436f6b1bc622bc6e5890
chris-reeves1 <91483522+chris-reeves1@users.noreply.github.com>
2025-10-15 15:20:09 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,10 @@ output "ec2_public_ip" {
 }
 
 output "s3_bucket_name" {
-  value = module.s3.bucket
+  value = module.s3.s3_bucket_id
 }
 
 output "vpc_public_subnet" {
   value = module.vpc.public_subnet_id
+
 }
